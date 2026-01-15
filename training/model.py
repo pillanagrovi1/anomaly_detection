ebaa7b4d9b26c5fc1e4b87c275433f31de54cd25
Pillanagrovi Rahul <156745142+pillanagrovi1@users.noreply.github.com>
2026-01-15 09:20:24 +0530

    Update model.py

--- training/model.py
+++ training/model.py
@@ -1,16 +1,20 @@
 import torch
 import torch.nn as nn
 
-class AnomalyModel(nn.Module):
+class AnomalyClassifier(nn.Module):
     def __init__(self, input_dim=5, num_classes=2):
         super().__init__()
-        self.net = nn.Sequential(
+
+        self.features = nn.Sequential(
             nn.Linear(input_dim, 16),
             nn.ReLU(),
             nn.Linear(16, 8),
-            nn.ReLU(),
-            nn.Linear(8, num_classes)
+            nn.ReLU()
         )
 
+        self.classifier = nn.Linear(8, num_classes)
+
     def forward(self, x):
-        return self.net(x)
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
